f4c9b261b054f70f9a3b8ebbf24aa0c0e4a69435
Sara Zhou <sara00915@gmail.com>
2024-08-21 14:53:02 +1000

    finalize homepage views with data and filters

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,15 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 import os
 
-
-
-
-
-load_dotenv()
-
-
+load_dotenv()# Load environment variables from .env file
 
 # connect to database using env variable
 engine = create_engine(f"postgresql+psycopg2://postgres:{os.getenv('DB_PW')}@localhost/python_news_db")
@@ -19,3 +14,21 @@ engine = create_engine(f"postgresql+psycopg2://postgres:{os.getenv('DB_PW')}@loc
 
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+  Base.metadata.create_all(engine)
+
+  app.teardown_appcontext(close_db)
+
+def get_db():
+  if 'db' not in g:
+    # store db connection in app context
+    g.db = Session()
+
+  return g.db
+
+def close_db(e=None):
+  db = g.pop('db', None)
+
+  if db is not None:
+    db.close()
